09832c92ae354c57ac40835b2a6be28434defa6c
StKorableva <st.korableva@yandex.ru>
2021-03-29 16:01:52 +0300

    add editorconfig and fix eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <p>ROD will be here! August 2021 :)</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>ROD will be here! August 2021 :)</p>
+		</div>
+	);
 }
 
 export default App;
