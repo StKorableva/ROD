43e2615277cca47f80384f989820f453ebe68f3c
Matthew <Matbei14@yandex.ru>
2021-05-15 00:32:02 +0300

    add component/add test styles/add base test/add a little love + some squash

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<p>ROD will be here! August 2021 :)</p>
+			<Footer></Footer>
 		</div>
 	);
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import './Footer.css';
+import twitterIcon from '../../img/twitter.svg';
+import instagramIcon from '../../img/instagram.svg';
+import facebookIcon from '../../img/facebook.svg';
+
+function Footer(props) {
+	function getCurrentYear() {
+		return new Date().getFullYear();
+	}
+	return (
+		<div className="footer">
+			<div className="footer__textBox">
+				<p className="footer__textBox__heading">ROD</p>
+				<p className="footer__textBox__copyright">© {getCurrentYear()} Some amazing team</p>
+			</div>
+			<div className="footer__socials">
+				<img className="footer__socials__twitter" src={twitterIcon} alt=""></img>
+				<img className="footer__socials__facebook" src={facebookIcon} alt=""></img>
+				<img className="footer__socials__instagram" src={instagramIcon} alt=""></img>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
