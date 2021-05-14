68cba8c5b4c7f6031916ac101e43e4dfd261edc2
Matthew <Matbei14@yandex.ru>
2021-05-15 01:58:11 +0300

    eslint fucking fuck/ add base test/add :* now/i want to die with my teddy bear/i too drink/im too tired/im sorry

--- src/__tests__/Foter.test.js
+++ src/__tests__/Foter.test.js
@@ -1,18 +1,17 @@
 import Enzyme, { shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import { shallow } from "enzyme";
 import toJSON from "enzyme-to-json";
-
-import Footer form '../components/Footer/Footer';
+import Adapter from "enzyme-adapter-react-16";
+import Footer from "../components/Footer/Footer";
 
 Enzyme.configure({ adapter: new Adapter() });
+describe("base test", () => {
+	let component = shallow(<Footer></Footer>);
 
-describe("base test",()=>{
-    let component = shallow(<Footer/>);
-    it("render test",()=>{
-        expect(component).toHaveLength(1);
-    })
-    it("snapshot test",()=>{
-        expect(toJSON(component)).toMatchSnapshot();
-    })
-})
+	it("render test", () => {
+		expect(component).toHaveLength(1);
+	});
+
+	it("snapshot test", () => {
+		expect(toJSON(component)).toMatchSnapshot();
+	});
+});
