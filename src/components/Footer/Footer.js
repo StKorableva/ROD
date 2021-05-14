ae686d614791e10d2d1f5dec998bc28d91f822db
Matthew <Matbei14@yandex.ru>
2021-05-15 01:02:45 +0300

    missing )

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,13 @@
 import "./Footer.css";
 
-function Footer(props){
-    return(<div className="footer">
-        <p className="footer__name">
-Where the fucking footer in the mobile version?
-        </p>
-    </div>
+function Footer(props) {
+	return (
+		<div className="footer">
+			<p className="footer__name">
+				Where the fucking footer in the mobile version?
+			</p>
+		</div>
+	);
 }
 
 export default Footer;
