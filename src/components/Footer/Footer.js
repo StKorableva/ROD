99ea9ffc6162e39679190dcda48a4bcb7000dbca
Matthew <Matbei14@yandex.ru>
2021-05-15 01:00:03 +0300

    add footer base code

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer(props){
+    return(<div className="footer">
+        <p className="footer__name">
+Where the fucking footer in the mobile version?
+        </p>
+    </div>
+}
+
+export default Footer;
