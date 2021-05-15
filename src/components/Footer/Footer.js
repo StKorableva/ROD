fcd927cf066bc830bcde9622a49da26df5d451d6
Matthew <Matbei14@yandex.ru>
2021-05-15 21:11:21 +0300

    add socials svg to img dirrectory+add socials img to component/fixed spelling error - curent=>current/

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,21 @@
 import './Footer.css';
+import twitterIcon from '../../img/twitter.svg';
+import instagramIcon from '../../img/instagram.svg';
+import facebookIcon from '../../img/facebook.svg';
 
 function Footer(props) {
-	function getCurentYear() {
+	function getCurrentYear() {
 		return new Date().getFullYear();
 	}
 	return (
 		<div className="footer">
 			<p className="footer__heading">READ OR DIE</p>
-			<p className="footer__copyright">© {getCurentYear()} Some amazing team</p>
+			<p className="footer__copyright">© {getCurrentYear()} Some amazing team</p>
+			<div className="footer__socials">
+				<img className="footer__socials__twitter" src={twitterIcon} alt=""></img>
+				<img className="footer__socials__facebook" src={facebookIcon} alt=""></img>
+				<img className="footer__socials__instagram" src={instagramIcon} alt=""></img>
+			</div>
 		</div>
 	);
 }
