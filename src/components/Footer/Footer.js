cf6a22eafd273383e1345433df84530c3c6e4833
Matthew <Matbei14@yandex.ru>
2021-05-15 20:53:12 +0300

    add heading/copyright line

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,13 @@
 import './Footer.css';
 
 function Footer(props) {
+	function getCurentYear() {
+		return new Date().getFullYear();
+	}
 	return (
 		<div className="footer">
-			<p className="footer__name">Where the fucking footer in the mobile version?</p>
+			<p className="footer__heading">READ OR DIE</p>
+			<p className="footer__copyright">© {getCurentYear()} Some amazing team</p>
 		</div>
 	);
 }
