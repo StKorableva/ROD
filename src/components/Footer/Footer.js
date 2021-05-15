2cee35ade554f11738c9ce8f6e27bc7c1f8a1ca0
Matthew <Matbei14@yandex.ru>
2021-05-15 15:39:45 +0300

    component fix for working with eslint...

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,10 @@
-import "./Footer.css";
+/* eslint-disable no-debugger, no-console */
+import './Footer.css';
 
 function Footer(props) {
 	return (
 		<div className="footer">
-			<p className="footer__name">
-				Where the fucking footer in the mobile version?
-			</p>
+			<p className="footer__name">Where the fucking footer in the mobile version?</p>
 		</div>
 	);
 }
