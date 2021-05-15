8aedf1220e8ee37b607ee356caf7da881b5f6ce7
Matthew <Matbei14@yandex.ru>
2021-05-15 20:59:31 +0300

    add addToFavorites heart icon/css test style

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,4 +1,5 @@
 import './ProductCard.css';
+import heartIcon from '../../img/heart-icon.svg';
 
 function ProductCard(props) {
 	let addToFavoritesClickHandler = function (e) {
@@ -7,7 +8,9 @@ function ProductCard(props) {
 	};
 	return (
 		<div className="productCard" onClick={props.openProductPage}>
-			<div className="productCard__addToFavorites" onClick={addToFavoritesClickHandler}></div>
+			<div className="productCard__addToFavorites" onClick={addToFavoritesClickHandler}>
+				<img className="productCard__addToFavorites__icon" src={heartIcon} alt=""></img>
+			</div>
 			<img className="productCard__img" src={props.url} alt="houston we have a problems"></img>
 			<p className="productCard__text">{props.text}</p>
 			<p className="productCard__price">{props.price}</p>
