0bdb759049ba0f1f1b6f72fabb7fc60aa39a9c66
Matthew <Matbei14@yandex.ru>
2021-05-21 16:42:18 +0300

    install/config eslint & prettier dependencies

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,12 +11,26 @@ function Footer(props) {
 		<div className="footer">
 			<div className="footer__textBox">
 				<p className="footer__textBox__heading">ROD</p>
-				<p className="footer__textBox__copyright">© {getCurrentYear()} Some amazing team</p>
+				<p className="footer__textBox__copyright">
+					© {getCurrentYear()} Some amazing team
+				</p>
 			</div>
 			<div className="footer__socials">
-				<img className="footer__socials__twitter" src={twitterIcon} alt=""></img>
-				<img className="footer__socials__facebook" src={facebookIcon} alt=""></img>
-				<img className="footer__socials__instagram" src={instagramIcon} alt=""></img>
+				<img
+					className="footer__socials__twitter"
+					src={twitterIcon}
+					alt="houston we have a problems"
+				></img>
+				<img
+					className="footer__socials__facebook"
+					src={facebookIcon}
+					alt="houston we have a problems"
+				></img>
+				<img
+					className="footer__socials__instagram"
+					src={instagramIcon}
+					alt="houston we have a problems"
+				></img>
 			</div>
 		</div>
 	);
