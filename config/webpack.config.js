d31fbe8751146631eb916908ca9cebbcb394e126
Владислав Бодин <vladislavbodin@MacBook-Pro-Vladislav.local>
2021-05-28 11:25:31 +0300

    Добавляет svg-loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -388,6 +388,10 @@ module.exports = function (webpackEnv) {
 								name: 'static/media/[name].[hash:8].[ext]',
 							},
 						},
+						{
+							test: /\.svg$/,
+							loader: 'svg-sprite-loader',
+						},
 						// Process application JS with Babel.
 						// The preset includes JSX, Flow, TypeScript, and some ESnext features.
 						{
